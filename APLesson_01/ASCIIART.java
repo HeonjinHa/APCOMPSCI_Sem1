3a8c5e07adb1321b1f751a8972d5f7dd238390f3
HeonjinHa <gkgjswls9849@naver.com>
2016-09-02 11:25:43 -0700

    Added Files

---
+++ APLesson_01/ASCIIART.java
@@ -0,0 +1,33 @@
+public class ASCIIART
+{
+	 public static void main(String[]args)
+	{
+		System.out.println("                               ");
+		System.out.println("                              .......................;    ");
+		System.out.println("                              ........................;  ");
+		System.out.println("                             ..........................;                       ");
+		System.out.println("                             ............................;                    ");
+		System.out.println("                             ............................;                             Y");
+		System.out.println("                                 WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW                                                    ");
+		System.out.println("                                WWWW                        WWWWWWWWWWWW                          WWWW                                                                      ");
+		System.out.println("                              WWWW                           WWWWWWWWWWWW                           WWWWW                                                                       ");
+		System.out.println("                             WWWW                             WWWWWWWWWWW                              WWWWW                                                                       ");
+		System.out.println("                             WWWW                              WWWWWWWWWWW                              WWWWWW                                                                       ");
+		System.out.println("                            WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW                                                                           ");
+		System.out.println("           HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH                                                                                        ");
+		System.out.println("           HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH                                                                                        ");
+		System.out.println("           HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH                                                                                        ");
+		System.out.println("           HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH                                                                                        ");
+		System.out.println("           HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH------                                                                                     ");
+		System.out.println("           HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH_________)                                                                                    ");
+		System.out.println("              IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII)))                                                           ");
+	    System.out.println("               I=============================================================))                                                       ");
+		System.out.println("                    I_______________________________________________________________________________)  ");
+		System.out.println("                                           ((@@@@))                                                       ((@@@@))       ");
+		System.out.println("                                        ((@@@@@@))                                                ((@@@@@@))                           ");
+		System.out.println("                                      ((@@@@@@@))       		               ((@@@@@@@))                                ");
+		System.out.println("                                        (@@@@@@@)           		                (@@@@@@@)                          ");
+		System.out.println("                                         ((@@@@@@))     		                 ((@@@@@@))                        ");
+		
+	}
+}
---
+++ APLesson_01/ASCIIARTII.java
@@ -0,0 +1,39 @@
+public class ASCIIARTII
+{
+	 public static void main(String[]args)
+	{
+		System.out.println("       ___________                            ___________                ");
+		System.out.println("      I                      I                          I                      I                    ");
+		System.out.println("      I                      I                          I                      I");
+		System.out.println("      I                      I                          I                      I");
+		System.out.println("      I     @              I                          I         @          I");
+		System.out.println("      I                      I                          I                      I");
+		System.out.println("      I                      I                          I                      I          ");
+		System.out.println("      I___________I                          I___________I                               ");
+		System.out.println("                                                      ");
+		System.out.println("                                                             ");
+		System.out.println("                                                   ");
+		System.out.println("                                                   ");
+		System.out.println("                                    ((( //// )))                   ");
+		System.out.println("                                    ((( //// )))                   ");
+		System.out.println("                                  (((( //// ))))                  ");
+		System.out.println("                                (((((( //// ))))))                   ");
+		System.out.println("                                (((((( ..///.. ))))))                   ");
+		System.out.println("                                                 ");
+		System.out.println("                                               ");
+		System.out.println("                                               ");
+		System.out.println("                                               ");
+		System.out.println("           @@@@@         @@@@@         @@@@@@@@                                      ");
+		System.out.println("             @@@@@@@@@@@@@@@@@@@@@@                                      ");
+		System.out.println("               @@@@@@@@@@@@@@@@@@@@@                               ");
+		System.out.println("                 @@@@@@@@@@@@@@@@@@@@                                ");
+		System.out.println("                                               ");
+		System.out.println("                                               ");
+		System.out.println("                                               ");
+
+		
+		
+		
+		
+	}
+}
