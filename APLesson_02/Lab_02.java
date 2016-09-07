9f88db06f6f7edffd1f83c37ae0eb36ca462b05f
HeonjinHa <gkgjswls9849@naver.com>
2016-09-07 12:05:15 -0700

    Lab 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,23 @@
+public class Lab_02 
+{
+	public static void main(String[]args)
+	{
+		int num1 = 8;
+		int num2 = 6;
+		int result = num1* num2; 
+		
+		System.out.println(num1 + " multiplied by " + num2 + " is " + result);
+		
+		int num3 = 12613;
+		int num4 = 92130;
+		
+		System.out.println(" Mikey Ha " + num3 + " el camino real D , San Diego " + num4);
+		
+		int num5 = 7;
+		int num6 = 3;
+		int num7 = 5;
+		int surArea= 2*(num5*num6)+2*(num6*num7)+2*(num7*num5);
+		
+		System.out.println( "The surface area of rectangle is " + surArea);
+	}
+}
