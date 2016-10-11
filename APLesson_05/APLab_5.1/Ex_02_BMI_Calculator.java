cc788d7f14727672e4f0f5ce1842e7caadd5c1ac
HeonjinHa <gkgjswls9849@naver.com>
2016-10-11 12:14:29 -0700

    lab 5

---
+++ APLesson_05/APLab_5.1/Ex_02_BMI_Calculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Ex_02_BMI_Calculator 
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("I am a BMI Calculator \nWhat is your height in?");
+		double height = kb.nextDouble();
+		System.out.println("And your weight?");
+		double weight = kb.nextDouble(); 
+		
+		double BMI 
+		System.out.println("Your BMI is... " + BMI);
+	}
+	
+	public static void calcBMI(double number)
+	{
+		if (grade)
+			{return "Condition is Underweight"}
+		if (grade.equals("b"))
+			{return "condition is  Normal" ;}
+		if (grade.equals("c"))
+			{return "condition is Overweight";}
+		if (grade.equals("d"))
+			{return "Condition is Obese";}
+		if (grade.equals("f"))
+			{return "Condition is Very Obese";}
+		if ()
+		{return "Morbidly Obese";}
+		else
+			return 0;
+	}
+}
+		
