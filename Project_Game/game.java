9276fc32c0b0c9d6142f1f7a5e15882cb6097800
HeonjinHa <gkgjswls9849@naver.com>
2017-05-25 12:12:05 -0700

    {P
    project

--- Project_Game/game.java
+++ Project_Game/game.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import java.util.*; //for ArrayList 
 public class game
 {
 	public static void main(String[] args)
--- Project_Game/gameFrame.java
+++ Project_Game/gameFrame.java
@@ -2,52 +2,65 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class gameFrame extends JFrame implements Keylistener, Runnable 
+//KeyListener for keyevents
+//Runnable for threads
+public class gameFrame extends JFrame implements KeyListener,Runnable
 {
-	Toolkit tk = Toolkit.getDefaultToolkit(); //toolkit for creating an image.
-	Image airplane = tk.getImage("plane.png"); //get an image of airplain.
-	int f_width = 795;
-	int f_height = 1042;
-	int x,y; 
-	boolean Keyup = false;
+	int x,y;
+	boolean KeyUp = false;
 	boolean KeyDown = false;
 	boolean KeyLeft = false;
 	boolean KeyRight = false;
-	Thread th; 
-	Toolkit tk = Toolkit.getDefaultToolkit(); Image airplane =tk.getImage("plane.png");
+	Thread th;	
+	
+	Toolkit tk = Toolkit.getDefaultToolkit(); //toolkit for creating an image.
+	Image airplane = tk.getImage("plane.png"); //get an image of airplane.
+	
 	gameFrame()
-	{	
+	{		
 		init(); //same as Frame class.
 		start(); //same as Frame class.
-		setTitle("Shooting Game");
-		setSize(f_width,f_height);
-		Dimension screen = tk.getScreenSize(); //Gets the size of the screen.
-		//To set the frame on the middle of the screen
 		
-		int fXPos = (int)screen.getWidth() / 2 - f_width / 2;
-		int fYPos = (int)screen.getHeight() / 2 - f_height/ 2;
+		setTitle("Shooting Game");
+		setSize(800,900);
+		
+		Dimension screen = tk.getScreenSize(); //Gets the size of the screen.
+		
+		//To set the frame on the middle of the screen
+		int fXPos = (int)screen.getWidth() / 2 - 800 / 2;
+		int fYPos = (int)screen.getHeight() / 2 - 900 / 2;
 		
 		setLocation(fXPos,fYPos);
-
+	
 		//make users not change the size
-		setResizable(false);
+		setResizable(true);
 		setVisible(true);
+		
+		
 	}
 	
 	public void init()
-	{	
-	   x=100;
-	   y=100;
+	{ 
+		int x = 100;
+		int y = 100;
+		
 	}
 	
 	public void start()
 	{
+		//sets the operation that will happen by default when the user initiates a "close" on this frame.
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		addKeyListerner(this); 
-		th = new Thread(this); 
-		th.start();
 		
+		addKeyListener(this);
+		th = new Thread(this);
+		th.start();
 	}
+	
+//What is thread?
+//   ->an independent path of execution within a program.
+//   ->Main thread processes our program only; CPU can process only one program at a time.
+//   ->If we put more threads on our program, we can execute multiple processes simultaneously.
+//   ->While mutiple threads would be helpful in executing many processes, but it might cause lags.
 	
 	public void run()
 	{
@@ -57,47 +70,82 @@ public class gameFrame extends JFrame implements Keylistener, Runnable
 			{
 				KeyProcess();
 				repaint();
-				Thread.sleep(20); 
-				
+				Thread.sleep(20);
 			}
-		}catch (Exception e){}
+		}
+		catch(Exception e)
+		{
+			
+		}
 	}
 	
 	public void paint(Graphics g)
 	{
 		//Clears the specified rectangle by filling it with 
 		//the background color of the current drawing surface.
-		g.clearRect(0,0,f_width,f_height);
+		g.clearRect(0,0,800,900);
 		
 		//Put the image of an airplane on (100,100).
-		g.drawImage(airplane,x,y,this); // this = imageobserver = hard to explain.
+		g.drawImage(airplane,x+350,y+750,this); // this = imageobserver = hard to explain.
+	}
+	
+	//Actions when the user presses control keys
+	public void keyPressed(KeyEvent e)
+	{
+		//switch statement is similar as if statement, but is used when we have a number of 
+		//possible excution paths. It works with multiple data types other than boolean.
+		switch(e.getKeyCode())
+		//getKeyCode method returns the event's keyCode
+		{
+			case KeyEvent.VK_UP:
+			KeyUp = true;
+			break;
+			case KeyEvent.VK_DOWN:
+			KeyDown = true;
+			break;
+			case KeyEvent.VK_LEFT:
+			KeyLeft = true;
+			break;
+			case KeyEvent.VK_RIGHT:
+			KeyRight = true;
+			break;
+		}
 	}
 
-	public void keyPressed(KeyEvent e)
+	//Actions after the user presses control keys
+	public void keyReleased(KeyEvent e)
 	{
 		switch(e.getKeyCode())
 		{
 			case KeyEvent.VK_UP:
-			KeyUp = true;
-			break; 
+			KeyUp = false;
+			break;
 			case KeyEvent.VK_DOWN:
-			KeyDown = true; 
-			break; 
+			KeyDown = false;
+			break;
 			case KeyEvent.VK_LEFT:
-			KeyLeft = true;
-			break; 
+			KeyLeft = false;
+			break;
 			case KeyEvent.VK_RIGHT:
-			KeyRight = true; 
+			KeyRight = false;
 			break;
 		}
 	}
-	public void keyTyped(KeyEvent e){}
+
+	
+	//Executed when the user types something
+	public void keyTyped(KeyEvent e)
+	{
+		
+	}
+	
+	//Actually moves the airplane as the user presses a key
 	public void KeyProcess()
 	{
-		if(KeyUp == true) y -=5; 
-		if(KeyDown == true) y +=5; 
-		if(KeyLeft == true) x -=5;
-		if(KeyRight == true) x +=5;
+		if(KeyUp == true) y -=10;
+		if(KeyDown == true) y +=10;
+		if(KeyLeft == true) x -=10;
+		if(KeyRight == true) x +=10;
 	}
 }
 
@@ -108,3 +156,5 @@ public class gameFrame extends JFrame implements Keylistener, Runnable
 //     -->setDefaultCloseOperation)(JFrame.EXIT_ON_CLOSE)
 //   https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics.html
 //     -->Graphics
+//   https://docs.oracle.com/javase/7/docs/api/java/awt/event/KeyEvent.html
+//     -->KeyEvent
